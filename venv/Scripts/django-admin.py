be158e18545c32fa1b41b10fbc5bebba3f28acd4
igor9041 <78731047+igor9041@users.noreply.github.com>
2021-02-28 01:32:45 +0300

    anfica

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\User\YandexDisk\Django TEST 2\DjangoWebProject1\DjangoWebProject1\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
